87b2d42421ff8797bf31aa044e7c898fcd94d617
henrydsantiago <henryuny@gmail.com>
2020-04-16 19:20:46 -0400

    Configuracion inicial completa del proyecto de React con npm, sus dependencias, babel, eslint y webpack

---
+++ src/components/HelloWorld.jsx
@@ -0,0 +1,8 @@
+import React from '../../node_modules/react'
+import '../assets/styles/App.scss'
+
+const HelloWorld = () =>(
+    <h1>Hola mundo</h1>
+)
+
+export default HelloWorld
---
+++ src/index.js
@@ -0,0 +1,5 @@
+import React from '../node_modules/react'
+import ReactDOM from '../node_modules/react-dom'
+import HelloWorld from './components/HelloWorld'
+
+ReactDOM.render(<HelloWorld />, document.getElementById("app"))
